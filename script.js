39927ecf2ce82cd07527871d28b390cd2c88f866
Noah <261243465+wimmernoah@users.noreply.github.com>
2026-04-20 19:13:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function animate() {
+  document.querySelectorAll('svg path').forEach((path, i) => {
+    const length = path.getTotalLength();
+    path.style.strokeDasharray = length;
+    path.style.strokeDashoffset = length;
+    path.style.animation = 'none';
+    setTimeout(() => {
+      path.style.animation = `draw 0.5s ease forwards ${i * 0.4}s`;
+    }, 50);
+  });
+}
+
+animate();
